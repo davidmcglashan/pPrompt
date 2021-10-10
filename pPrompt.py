95ab5e869cdd2a0d055435f8d068397f314c15ea
David McGlashan <5269729+davidmcglashan@users.noreply.github.com>
2021-10-10 16:42:15 +0100

    Commented getch()

--- pPrompt.py
+++ pPrompt.py
@@ -19,25 +19,35 @@ termAttr = termios.tcgetattr(0)
 #  Read a single character from the keyboard
 # =============================================
 def getch():
+	# Terminal magic reading the character into 'ch'
     tty.setraw(fdInput)
     ch = sys.stdin.buffer.raw.read(4).decode(sys.stdin.encoding)
 
+    # Some characters are 1 char long and therefore simple to deal with.
     if len(ch) == 1:
+    	# Ctrl+C quits the program. We do this as soon as possible to be a good citizen.
         if ord(ch[0]) == key_CtrlC:
             sys.exit(0)
+
+        # Characters less than 32 and greater than 126 aren't alphanumerics. We convert
+        # these to their ordinal counterparts for ease of working later.
         if ord(ch) < 32 or ord(ch) > 126:
             ch = ord(ch)
+
+    # Escape characters have 127 as their first character. This must be replaced
+    # with \033 for some reason.
     elif ord(ch[0]) == key_Esc:
         ch = '\033' + ch[1:]
 
+    # More terminal magic and then return our character.
     termios.tcsetattr(fdInput, termios.TCSADRAIN, termAttr)
     return ch
-
-sys.stdout.write( "\r> ")
 
 # ======================================
 #  Main loop
 # ======================================
+sys.stdout.write( "\r> ")
+
 while True:
 	# Get a single character
 	c = getch()
